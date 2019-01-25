5af04b549b7a6381b10d411b6dfadfe1d3e5b584
balajimanikandanm <mkbalaji1998@gmail.com>
2019-01-25 11:47:49 +0530

    Update Arraymaximum.java

--- Arraymaximum.java
+++ Arraymaximum.java
@@ -1,22 +1,23 @@
 import java.io.*;
-
-  public class Arraymaximum{
-    public static void main(String args[]){
-      int max;
-      int a[]={1,2,3,4,5};
-      max=a[0];
-      System.out.println("Max Element in an Array...");
-      for(int i=0;i<=a.length;i++)
-      {
-      int j=i+1;
-				if(j<x)
-				{
+public class Arraymaximum()
+{
+	public static void main(String args[])
+	{
+		int max;
+		int a[]={1,2,3,4,5};
+		max=a[0];
+		System.out.ptintln("Maximum element");
+		for(int i=0;i<=a.length;i++)
+		{
+			int j=i+1;
+			if(j<x)
+			{
 				if(a[i]<a[j])
-					{
-						max=a[j];
-					}
+				{
+					max=a[j];
 				}
 			}
 			System.out.println(max);
 		}
 	}
+}
