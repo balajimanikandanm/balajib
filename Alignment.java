2436c5a4d02297e054ced06e43d86c991ec90847
balajimanikandanm <mkbalaji1998@gmail.com>
2019-01-25 11:35:17 +0530

    Create Alignment.java

---
+++ Alignment.java
@@ -0,0 +1,26 @@
+import java.io.*;
+import java.util.*;
+public class Alignment
+{
+public static void main(String args[])
+{
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter any number:");
+        int in=sc.nextInt();
+        int a1,b1=0,c1,d1=0;
+        while(in>0)
+        {
+          a1=in%10;
+          b1=(b1*10)+a1;
+          in=in/10;
+         }
+         //Sysem.out.println(b1);
+        while(b1>0)
+        {
+        c1=b1%10;
+        d1=(d1*10)+c1;
+        b1=b1/10;
+        }
+        System.out.println(d1);
+      }
+    }  
