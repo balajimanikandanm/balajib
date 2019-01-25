db57cb598ff29a73e7e3c6994e624801913ff1f9
balajimanikandanm <mkbalaji1998@gmail.com>
2019-01-25 12:07:33 +0530

    Update Arraysort.java

--- Arraysort.java
+++ Arraysort.java
@@ -4,34 +4,30 @@ public class Arraysort
 {
 public static void main(String args[])
 {
- Scanner sc=new Scanner(System.in);
-      
-    
-      int temp;
-      int n=sc.nextInt();
-      int a[]=new int[n];
-      for(int i=0;i<n;i++)
-      {
-        a[i]=sc.nextInt();
-      }  
-        
-      
-      System.out.println("Sorted array");
-        for(int i=0;i<n;i++)
-        {
-          for(int j=i+1;j<n;j++)
-          {
-            if(a[i]>a[j])
-            {
-              temp=a[i];
-              a[i]=a[j];
-              a[j]=temp;
-            }
-           } 
-          }
-          for(int i=0;i<n;i++)
-          {
-          System.out.print(a[i]+" ");
-          }
-        }
-       } 
+ Scanner sc=new Scanner(System.in)
+  int temp;
+ int n=sc.nextInt();
+ int a[]=new int[n];
+ for(int i=0;i<n;i++)
+ {
+  a[i]=sc.nextInt();
+ }
+ for(i=0;i<n;i++)
+ {
+  for(int j=i+1;j<n;j++)
+  {
+   if(a[i]>a[j])
+   {
+    temp=a[i];
+    a[i]=a[j];
+    a[j]=temp;
+   }
+  }
+ }
+ for(int i=0;i<n;i++)
+ {
+  System.out.print(a[i]+"");
+ }
+}
+}
+   
