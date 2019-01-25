78e700860509286f8b810f081f3a0b0e4e43ae8d
balajimanikandanm <mkbalaji1998@gmail.com>
2019-01-25 21:45:19 +0530

    Update StringEx.java

--- StringEx.java
+++ StringEx.java
@@ -2,12 +2,12 @@ import java.io.*;
 import java.util.*;
   public class StringEx{
     public static void main(String args[]){
-      Scanner sc=new Scanner(System.in);
+      Scanner s=new Scanner(System.in);
       System.out.println("Enter Any String");
-      String in=sc.nextLine();
+      String in=s.nextLine();
       System.out.println("Enter how many times the string to be display:");
-      int range=sc.nextInt();
-      for(int i=0;i<range;i++)
+      int r=s.nextInt();
+      for(int i=0;i<r;i++)
       {
         System.out.println(in);
       }
