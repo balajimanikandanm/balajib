de913529ef03e0dab2dd97a768443b758a68cf12
balajimanikandanm <mkbalaji1998@gmail.com>
2019-02-01 11:39:22 +0530

    Create SwapChar.java

---
+++ SwapChar.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.util.*;
+public class SwapChar
+{
+public static void main(String args[])
+{
+StaringBuffer sb=new StringBuffer();
+String a;
+int i;
+char c,d;
+Scanner s=new Scanner(System.in);
+a= s.next();
+for(i=0;i<a.length()-1;i=i+2)
+{
+c=a.charAt(i);
+d=a.charAt(i+1);
+sb.append(b).append(a);
+}
+System.out.println(sb);
+}
+}
