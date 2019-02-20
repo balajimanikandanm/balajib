2e965e3e5ca1540040662f200e072bf113d296aa
balajimanikandanm <mkbalaji1998@gmail.com>
2019-02-20 14:28:03 +0530

    Create PrimeBetween.java

---
+++ PrimeBetween.java
@@ -0,0 +1,31 @@
+import java.io.*;
+import java.util.*;
+public class PrimeBetween
+{
+public static void main(String[] args)
+{
+int l,r,countt=0,i,j;
+Scanner sc=new Scanner(System.in);
+l = sc.nextInt();
+r = sc.nextInt();
+for(i=l;i<=r;i++)
+{
+for(j=2;j<i;j++)
+{
+if(i % j == 0)
+				{
+					countt = 0;
+					break;
+				}
+				else
+				{
+					countt = 1;
+				}
+			}
+			if(countt == 1)
+			{
+				System.out.println(i);
+			}
+		}
+	}
+}
