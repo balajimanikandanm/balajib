aac27b2eea470688277ed12b09d56ad1c6af5c59
balajimanikandanm <mkbalaji1998@gmail.com>
2019-02-20 13:57:56 +0530

    Create Camelcase.java

---
+++ Camelcase.java
@@ -0,0 +1,24 @@
+import java.io.*;
+import java.util.*;
+public class Camelcase
+{
+public static void main(String[] args)
+{
+Scanner s=new Scanner(System.in);
+System.out.print("Enter a sentence: ");
+        String input = s.nextLine();
+        String result = "";
+        char first = input.charAt(0);
+        result = result + Character.toUpperCase(first);
+        for (int i = 1; i < input.length(); i++) {
+            char currentChar = input.charAt(i);
+            char previousChar = input.charAt(i - 1);
+            if (previousChar == ' ') {
+                result = result + Character.toUpperCase(currentChar);
+            } else {
+                result = result + currentChar;
+            }
+        }
+        System.out.println(result);
+        }
+        }
