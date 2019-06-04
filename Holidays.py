a2fc25d4ee0bcc4abe16fa298d36854202c529c2
balajimanikandanm <mkbalaji1998@gmail.com>
2019-06-04 10:56:25 +0530

    Update Holidays.py

--- Holidays.py
+++ Holidays.py
@@ -1,7 +1,7 @@
 day=input()
 if day== "Monday"or"Tuesday"or"Wednesday"or"Thursday"or"Friday":
   return no
-elif day== "Saturday" or "Sunday"
+elif day== "Saturday" or "Sunday":
   return yes
 else:
  print("invalid")
